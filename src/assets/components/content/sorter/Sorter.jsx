feba9b977ed7f155ff40ba048774b34b3eafbd1c
unknown <pakelvi0707@gmail.com>
2021-10-12 12:18:56 +0300

    select edits

--- src/assets/components/content/sorter/Sorter.jsx
+++ src/assets/components/content/sorter/Sorter.jsx
@@ -1,12 +1,6 @@
 import s from '././Sorter.module.scss';
 import SorterItem from './sorterItem/SorterItem';
 
-
-import Box from '@mui/material/Box';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-
 import tile from './../../../img/tile.svg';
 import list from './../../../img/list.svg';
 
@@ -25,13 +19,20 @@ const data4 = {
     title: "еще"
 }
 const data5 = {
-    img: tile
+    title: "По популярности"
 }
 const data6 = {
+    title: "По цене"
+}
+const data7 = {
+    title: "По рейтингу"
+}
+const data8 = {
+    img: tile
+}
+const data9 = {
     img: list
 }
-
-
 
 function Sorter(props) {
     return (
@@ -46,22 +47,21 @@ function Sorter(props) {
             </nav>
 
             <div className={s.selectWrap}>
-                <Box sx={{width: 178}}>
-                    <FormControl fullWidth>
-                        <Select>
-                            <MenuItem value="По популярности">По популярности</MenuItem>
-                            <MenuItem value="По цене">По цене</MenuItem>
-                            <MenuItem value="По рейтингу">По рейтингу</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Box>
+
+                <select className={s.select}>
+                    <option value="По популярности">{data5.title}</option>
+                    <option value="По цене">{data6.title}</option>
+                    <option value="По рейтингу">{data7.title}</option>
+                </select>
+
+
 
                 <a className={s.sortTile}>
-                    <img src={data5.img} alt="" />
+                    <img src={data8.img} alt="" />
                 </a>
 
                 <a className={s.sortList}>
-                    <img src={data6.img} alt="" />
+                    <img src={data9.img} alt="" />
                 </a>
 
             </div>
