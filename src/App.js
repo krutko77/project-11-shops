cd7a2a23dd948954b9969cc06bb43bbb89ad7656
Mari-marina <shunkina9292@mail.ru>
2021-10-13 08:10:27 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import "./App.css";
 import Header from "./assets/components/header/Header.jsx";
-import Filter from "./assets/components/content/filter/Filter";
-import Navigation from "./assets/components/content/navigation/Navigation";
+import Main from "./assets/components/main/Main.jsx";
+import Catalog from "./assets/components/catalog/Catalog.jsx";
+import ProductCard from "./assets/components/product-card/ProductCard.jsx";
+import Footer from "./assets/components/footer/Footer.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-import Typography from "@mui/material/Typography";
-import Breadcrumbs from "@mui/material/Breadcrumbs";
-import Link from "@mui/material/Link";
-import Sorter from "./assets/components/content/sorter/Sorter";
-
-const data = {
-  heading: "Гидроциклы",
-};
-
-function App(props) {
+function App() {
   return (
-    <div className="container">
+    <BrowserRouter>
       <Header />
-      <Navigation />
-
-      <Breadcrumbs separator="›" aria-label="breadcrumb">
-        <Link underline="hover" color="#C4C4C4" href="/">
-          Главная
-        </Link>
-        <Typography color="#C4C4C4">Гидроциклы</Typography>
-      </Breadcrumbs>
-
-      <h1 className="heading">{data.heading}</h1>
-
-      <Sorter />
-
-      <Filter />
-    </div>
+      <Main />
+      <Catalog />
+      <ProductCard />
+      <Footer />
+    </BrowserRouter>
   );
 }
 
