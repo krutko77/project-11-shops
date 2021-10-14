8965e27a242d130f746eba39e1c4d36d17a1a8ad
krutko77 <70036691+krutko77@users.noreply.github.com>
2021-10-14 13:39:44 +0300

    Main.jsx

--- src/assets/components/main/Main.jsx
+++ src/assets/components/main/Main.jsx
@@ -1,8 +1,27 @@
 import s from "./Main.module.scss";
+import SliderBanner from './slider-banner/SliderBanner.jsx';
+import PriceAction from './price-action/PriceAction.jsx';
+import Search from './search/Search.jsx';
+import SectionProduct from './section-product/SectionProduct';
+import PopularGoods from './popular-goods/PopularGoods';
+import Banner from './banner/Banner';
+import BuyMore from './buy-more/BuyMore.jsx';
 
-function Main() {
+function Main(props) {
    return (
-     <div></div>
+     <main>
+        <div className={s.container}>
+            <div className={s.SliderBanner}>
+               <SliderBanner/>
+               <PriceAction/>
+            </div>
+            <Search/>
+            <SectionProduct/>
+            <PopularGoods/>
+            <Banner/>
+            <BuyMore/>
+        </div>        
+     </main>
    );
  }
  
