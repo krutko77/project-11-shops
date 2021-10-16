34b2b908bf90c7d11fb75fa244fbf633d00018ea
krutko77 <70036691+krutko77@users.noreply.github.com>
2021-10-16 14:12:03 +0300

    <Footer/> correct

--- src/assets/components/footer/Footer.jsx
+++ src/assets/components/footer/Footer.jsx
@@ -8,18 +8,16 @@ import Social from './social/Social';
 
 function Footer() {
    return (
-    <footer className={s.footer}>
-    <div className="container">
-        <div className={s.inner}>
-            <Mailing/>
-            <InfoMenu/>
-            <ShopMenu/>
-            <Social/>
-        </div>
-        <Contract/>
-    </div>
-</footer>
+      <footer className={s.footer}>
+         <div className={s.inner}>
+            <Mailing />
+            <InfoMenu />
+            <ShopMenu />
+            <Social />
+         </div>
+         <Contract />
+      </footer>
    );
- }
- 
- export default Footer;
+}
+
+export default Footer;
