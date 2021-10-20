82a4ec81cdb3a69647edcde4c4600b6e8c47ce23
bonusloop <denis7sined@gmail.com>
2021-10-20 19:47:28 +0300

    “”

--- src/assets/components/footer/Footer.jsx
+++ src/assets/components/footer/Footer.jsx
@@ -9,6 +9,7 @@ import Social from './social/Social';
 function Footer() {
    return (
       <footer className={s.footer}>
+         <div className={s.container}>
          <div className={s.inner}>
             <Mailing />
             <InfoMenu />
@@ -16,6 +17,7 @@ function Footer() {
             <Social />
          </div>
          <Contract />
+         </div>
       </footer>
    );
 }
