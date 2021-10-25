5d6095c6996a2338ff16628e2e54b38f21165bdb
unknown <pakelvi0707@gmail.com>
2021-10-25 17:08:56 +0300

    slider down

--- src/assets/components/main/block-various-goods/BlockVariousGoods.jsx
+++ src/assets/components/main/block-various-goods/BlockVariousGoods.jsx
@@ -2,14 +2,10 @@ import s from "./BlockVariousGoods.module.scss";
 
 import TitleBlock from "./title-block/TitleBlock";
 import SliderProduct from "./slider-goods/SliderProduct";
-import NavigationBlock from "./navigation-block/NavigationBlock";
 import ButtonBlock from "./button-block/ButtonBlock";
 
 
 
-// const data = {
-//   title: "Популярные товары"
-// }
 
 
 function BlockVariousGoods(props) {
@@ -17,7 +13,7 @@ function BlockVariousGoods(props) {
       <div className={s.blockVariousGoods}>
          <TitleBlock title={props.title} />
          <SliderProduct cardsData={props.cardsData} />
-         <ButtonBlock style={props.style} btnData={props.btnData}/>
+         <ButtonBlock style={props.btnStyle} btnData={props.btnData}/>
       </div>
    );
 }
