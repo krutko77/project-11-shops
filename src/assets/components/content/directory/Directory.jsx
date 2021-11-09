efbcfc74de57f3732ea1b5f293cc455c67cea8b0
Mari-marina <shunkina9292@mail.ru>
2021-11-09 22:00:06 +0300

    price

--- src/assets/components/content/directory/Directory.jsx
+++ src/assets/components/content/directory/Directory.jsx
@@ -13,15 +13,49 @@ import SliderProductCard from "../../../components/main-page/block-various-goods
 function Directory(props) {
   return (
     <div className={style.products}>
-      <SliderProductCard img={imgSlider2Card1} />
-      <SliderProductCard img={imgSlider2Card2} />
-      <SliderProductCard img={imgSlider2Card3} />
-      <SliderProductCard img={imgSlider2Card4} />
-      <SliderProductCard img={imgSlider2Card5} />
-      <SliderProductCard img={imgSlider2Card6} />
-      <SliderProductCard img={imgSlider2Card7} />
-      <SliderProductCard img={imgSlider2Card8} />
-      <SliderProductCard img={imgSlider2Card12} />
+      <SliderProductCard
+        img={imgSlider2Card1}
+        title="Гидроцикл BRP SeaDoo GTI 130hp SE Black\Mango"
+      />
+      <SliderProductCard
+        img={imgSlider2Card2}
+        title="Гидроцикл BRP SeaDoo GTI 155hp SE Long Blue Metallic"
+        price="1 543 000 ₽"
+      />
+      <SliderProductCard
+        img={imgSlider2Card3}
+        title="Гидроцикл BRP SeaDoo GTR 230hp X California green "
+        price="732 345 ₽"
+      />
+      <SliderProductCard
+        img={imgSlider2Card4}
+        title="Гидроцикл BRP SeaDoo GTR 230hp STD Black / Gulfstream"
+      />
+      <SliderProductCard
+        img={imgSlider2Card5}
+        title="Гидроцикл BRP SeaDoo GTX 300hp LTD Liquid Metal"
+        price="1 543 000 ₽"
+      />
+      <SliderProductCard
+        img={imgSlider2Card6}
+        title="Гидроцикл BRP SeaDoo Spark 60hp 2 up"
+        price="732 345 ₽"
+      />
+      <SliderProductCard
+        img={imgSlider2Card7}
+        title="Гидроцикл BRP SeaDoo Spark GTS 90hp Rental"
+        price="857 666 ₽"
+      />
+      <SliderProductCard
+        img={imgSlider2Card8}
+        title="Гидроцикл BRP SeaDoo WAKE 230hp PRO Teal blue"
+        price="1 229 711 ₽"
+      />
+      <SliderProductCard
+        img={imgSlider2Card12}
+        title="Гидроцикл Spark 2-UP 900 Ho Ace Chili Pepper"
+        price="587 440 ₽"
+      />
     </div>
   );
 }
