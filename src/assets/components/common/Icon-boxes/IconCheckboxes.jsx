f6d3b6bb19fac7141034ceadf1eec4543fb5311c
Mari-marina <shunkina9292@mail.ru>
2021-11-17 23:08:28 +0300

    catalog add

--- src/assets/components/common/Icon-boxes/IconCheckboxes.jsx
+++ src/assets/components/common/Icon-boxes/IconCheckboxes.jsx
@@ -1,35 +1,29 @@
 import s from "./IconCheckboxes.module.scss";
-import Checkbox from '@mui/material/Checkbox';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
-import { styled } from '@mui/material/styles';
-
+import Checkbox from "@mui/material/Checkbox";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
+import { styled } from "@mui/material/styles";
 
 // стилизация маржина и размера иконки
 const MyCheckbox = styled(Checkbox)({
-   marginLeft: -10
-}
-);
+  marginLeft: -10,
+  padding: 0,
+});
 
 const MyFavoriteBorder = styled(FavoriteBorder)({
-   fontSize: 30
-}
-);
+  fontSize: 30,
+});
 
 const MyFavorite = styled(Favorite)({
-   fontSize: 30
-}
-);
+  fontSize: 30,
+});
 
 function IconCheckboxes() {
-   return (
-      <div>
-         <MyCheckbox icon={<MyFavoriteBorder />} checkedIcon={<MyFavorite />} />
-      </div>
-   );
+  return (
+    <div>
+      <MyCheckbox icon={<MyFavoriteBorder />} checkedIcon={<MyFavorite />} />
+    </div>
+  );
 }
 
 export default IconCheckboxes;
-
-
-
